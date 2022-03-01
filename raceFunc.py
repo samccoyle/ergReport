c6931b2754919ef696856cd824598e4cad04627a
samccoyle <samccoyle@gmail.com>
2022-03-01 11:08:19 -0700

    excel fiel export works, weights are added, needs weight factor and correction, needs multiple json files

--- raceFunc.py
+++ raceFunc.py
@@ -1,25 +1,62 @@
+import numpy as np
+import os
+import sys
+import pandas as pd
+import glob
+from pathlib import Path
+import json
 
 def fileNames():
-	#	find all files ending in a certain extension
 	files = []
-	for file in os.listdir(folder):
-		if file.endswith('.JSON'):
-			files += file
+	try:
+		#check for a drag and drop file
+	    files += sys.argv[1]
+	except:
+		#	find all files ending in a certain extension
+		folder = Path().absolute()
+		print(folder)
+		for file in glob.glob("*.json"):
+			files.append(file)
+		for file in glob.glob("*.JSON"):
+			files.append(file)
+	print(files)
 	return files
+
+def expand_splits(splits_col):
+	flat_dicts = []
+	for splits in splits_col: #iterating through each row of the column
+	    temp_dic = {}
+	    for split in splits: #iterating thorugh each split segment
+	        running_dist = str(split['split_running_distance'])
+	        for key in split: # changing the keys to match the current completed distance for future expansion
+	            new_key  = key + '_' + running_dist
+	            temp_dic[new_key] = split[key]
+	    flat_dicts.append(temp_dic)
+	return flat_dicts
+
+def import_df(filename):
+	#create a data frame from a file 
+	f = open(filename)
+	data_import = json.load(f)
+	data = pd.json_normalize(
+	    data_import,
+	    record_path=['results' , 'participants'])
+
+	#expanding the list of dicts in the splits column
+	flat_dicts = expand_splits(data['splits'])
+	expanded_df = pd.DataFrame(flat_dicts)
+
+	#merging the expanded data into the previous data
+	names = data['participant']
+	names.columns = ['participant']
+	expanded_df.insert(0, 'participant', names)
+	del data['splits']
+	final_df = data.merge(expanded_df, how='left' , left_on=['participant'], right_on=['participant'])
+	return final_df
 
 def squads():
  #return: men, women, novice varsity 
 	return 0 
-
-def weightImport(names):
-	try:
-		weightData = pd.read_excel('weights.xlsx')
-	except:
-		raise Exception("Missing weight data")
-	weightData.sort_values(by=['Name'])
-	#check names
-	for index, row in weightData.iterrows():
-		print("Hello")
 
 
 
--- report2k.py
+++ report2k.py
@@ -2,72 +2,57 @@ import os
 import sys
 import numpy as np
 from datetime import datetime as dt
-from fpdf impoty FPDF
-import matlibplot.pyplot as plt
+from fpdf import FPDF
+import matplotlib.pyplot as plt
 from matplotlib import rcParams
 import statistics as stat
 import pandas as pd
-import 2kHelper as 2k
-
+from raceFunc import *
+from datetime import datetime
 
 # get data
-columnHeaders = []
-2kData = pd.DataFrame(columnHeaders)
-dataFilename = ""
+input_filenames = fileNames()
 
-try:
-	droppedFile = sys.argv[1]
-	2kData.append(pd.read_json(droppedFile))
-	dataFilename = droppedFile
-except:
-	files = collectFiles.names()
-	for name in files:
-		tempData = pd.read_json(name)
-		tempColumnHeaders = 2kData.columns.values
-		if list(tempColumnHeaders) == columnHeaders:
-			2kData.append(tempData)
-		else:
-			print("Skipping {0}, incorrect filetype", name)
-	dataFIlename = "2k Results"
-
-# slicing data to remove unnecessary 
-sliceParticipants(2kData)
-
-
+#add data to data frame
+df = pd.DataFrame()
+for file in input_filenames:
+	temp_df = import_df(file)
+	if df.empty:
+		df = temp_df
+		print('Replaced')
+	else:
+		df = pd.concat([df,temp_df],ignore_index=True)
+		print('Added')
+print(df.head(20))
 
 # add weight data since it isn't in the 2k results 
-
 try:
-	weightData = pd.read_excel('weights.xlsx')
-	2kData.merge(weightData[['Weight', 'Name']], how = 'left')
+	weightData = pd.read_excel('Weights.xlsx')
+	print("Weight Data")
+	print(weightData.head(20))
+	df.merge(weightData, how='left', left_on='participant', right_on='Name')
 except:
-	raise Exception("Missing weight data")
+	print('Failed to add weight data')
 
 
+# add calculations to 2k results
+print('hello')
+#not using to_datetime since %m%d%y %H data is missing
+temp = df.loc[:, df.columns.str.startswith(str('split_avg_pace_'))].applymap(lambda index:datetime.strptime(index, "%M:%S.%f"))
+df.loc[:, df.columns.str.startswith(str('split_avg_pace_'))] = temp
+
+df['stdDev500'] = df.loc[:, df.columns.str.startswith(str('split_avg_pace_'))].std(axis=1)
+print(df.head(20))
+#print(temp.head(20))
+#['stdDev500']
+df.to_excel('results.xlsx')
+
+with pd.ExcelWriter("results.xlsx", if_sheet_exists="replace") as writer:
+	df.to_excel(writer)
 
 
-#2kData.head()
-
-2k.weights
-
-# plot manipulation
-rcParams['axes.spines.top'] = False
-rcParams['axes.spines.right'] = False
-
-def plot(data: pd.DataFrame, filename: str) -> None:
-	plt.figure(figsize=(12, 4))
-	plt.grid(color='#F2F2F2', alpha=1, zorder=0)
-	plt.plot(data['Name'], data['500'],data['1000'],data['1500'],data['2000'], color='#087E8B', lw=3, zorder=5)
-	plt.title(f'2k Pace', fontsize=17)
-	plt.xlabel('Distance [m]', fontsize=13)
-	plt.xticks(fontsize=9)
-	plt.ylabel('Pace [sec/500m]', fontsize=13)
-	plt.yticks(fontsize=9)
-	plt.savefig(filename, dpi=300, bbox_inches='tight', pad_inches=0)
-	plt.close()
-	return
-
-#plot 2ks by eight 
+"""
+plot 2ks by eight
 2kData.sort_values([''])
 athleteCount= = len(2kData.index)
 currentIndex = 0
@@ -76,7 +61,7 @@ for ii in range(athleteCount):
 	filename = dataFilename + " " + currentIndex "-" + (currentIndex + 7)
 	plot(plotData,filename)
 	currentIndex += 8
-
+"""
 
 
 
