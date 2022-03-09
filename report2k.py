07bb11f00cc49c43ffa5471f345dae2bc1f753ab
samccoyle <samccoyle@gmail.com>
2022-03-09 09:13:11 -0700

    clean up of report2k for extraneous code, update requirements

--- report2k.py
+++ report2k.py
@@ -1,23 +1,21 @@
 import sys
 import numpy as np
 from datetime import datetime as dt
-from fpdf import FPDF
-import matplotlib.pyplot as plt
-from matplotlib import rcParams
-import statistics as stat
+#from fpdf import FPDF
+#import matplotlib.pyplot as plt
+#from matplotlib import rcParams
+#import statistics as stat
 import pandas as pd
 from raceFunc import *
 from datetime import datetime, timedelta
 
-#constansts
-cox_weight = 130
-boat_weight_8 = 200
-boat_weight_4 = 125
 
 # flags
 multi_file = False
 weight_data_exists = False
 
+
+## Adding data
 # get data
 input_filenames = fileNames()
 
@@ -36,59 +34,33 @@ try:
 	weightData = pd.read_excel('Weights.xlsx')
 	df = pd.merge(df, weightData, how='left', on='participant')
 	weight_data_exists = True
-	df['boat weight 4'] = df['weight'] + cox_weight/4 + boat_weight_4/4
-	df['boat weight 8'] = df['weight'] + cox_weight/8 + boat_weight_8/8
 	print("Added weights")
 except:
 	print('Failed to add weight data, skipping')
 
 
-#correcting time data
+#correcting time data units
 #not using to_datetime since %m%d%y %H data is missing
 temp = df.loc[:, df.columns.str.startswith(str('split_avg_pace_'))].applymap(lambda index:datetime.strptime(index, "%M:%S.%f"))
 df.loc[:, df.columns.str.startswith(str('split_avg_pace_'))] = temp
 df['time'] = df['time'].apply(lambda index:datetime.strptime(index, "%M:%S.%f"))
 df['avg_pace'] = df['avg_pace'].apply(lambda index:datetime.strptime(index, "%M:%S.%f"))
 
-# add calculations to 2k results
-
-# standard deviation data 
+## add calculations to 2k results
+# standard deviation data
 df['stdDev500'] = df.loc[:, df.columns.str.startswith(str('split_avg_pace_'))].std(axis=1)
 
 #weight conversion
 if weight_data_exists:
 
 	df['conversion factor'] = df['weight'].div(270).pow(0.222)
-	df['conversion factor 4']= df['boat weight 4'].div(270).pow(0.222)
-	df['conversion factor 8']= df['boat weight 8'].div(270).pow(0.222)
 
-	df['adjusted score'] = adjust_time(df['conversion factor'],df['time'])
-	df['4 score'] = adjust_time(df['conversion factor 4'],df['time'])
-	df['8 score'] = adjust_time(df['conversion factor 8'],df['time'])
+	df['adjusted time'] = adjust_time(df['conversion factor'],df['time'])
 
 	df['adjusted pace'] = adjust_time(df['conversion factor'],df['avg_pace'])
-	df['4 pace'] = adjust_time(df['conversion factor 4'],df['avg_pace'])
-	df['8 pace'] = adjust_time(df['conversion factor 8'],df['avg_pace'])
 
-# filtering data for saving
-string_to_filter = []
-"""
-['id',
-	'calories',
-	'class',
-	'lane',
-	'log',
-	'machine',
-	'serial',
-	'running'
-	'factor',
-	'count',
-	'distance',
-	'type',
-	'drag',
-	'runningtime',
-	'_time_']
-"""
+## Exporting data
+
 #filtering columns for excel
 filtered_df = filter_columns(df)
 filtered_df = filtered_df.sort_values(by=['score'])
@@ -101,6 +73,7 @@ with pd.ExcelWriter("results.xlsx", if_sheet_exists="replace") as writer:
 print("saved to excel")
 
 """
+TODO add plotting and export to pdf
 plot 2ks by eight
 2kData.sort_values([''])
 athleteCount= = len(2kData.index)
